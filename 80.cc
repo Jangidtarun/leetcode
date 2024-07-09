9b3e798076470d2587c11dce76247952b0f30d2e
Tarun Jangid <tarunjangid19102002@gmail.com>
2024-07-09 20:03:54 +0530

    found better method

--- 80.cc
+++ 80.cc
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
 int removeDuplicates(vector<int> &nums)
 {
     if (nums.size())
@@ -17,10 +18,26 @@ int removeDuplicates(vector<int> &nums)
     }
     return nums.size();
 }
+*/
+
+// arr = {1,1,1,2,2,3}
+//        0,1,2,3,4,5
+
+int removeDuplicates(vector<int> &nums){
+    int n = nums.size();
+    if(n < 3) return n;
+    int first = 0;
+    int counter = 1;
+    for(int i=0;i<n;i++){
+        if(first == 0 or first == 1 or nums[first-2] != nums[i])
+            nums[first++] = nums[i];
+    }
+    return first;
+}
 
 int main()
 {
-    vector<int> nums = {1, 1};
+    vector<int> nums = {1,2,3};
     int ans = removeDuplicates(nums);
     cout << ans << endl;
     for (auto val : nums)
